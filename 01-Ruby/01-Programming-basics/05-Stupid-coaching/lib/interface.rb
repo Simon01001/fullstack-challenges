68737fab8142fbf20e25171848f842302961e08c
szymon niedorezo <szymonniedorezo@gmail.com>
2024-08-20 16:27:44 +0100

    Add soution to the challenge

--- 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/interface.rb
+++ 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/interface.rb
@@ -1,2 +1,6 @@
 require_relative "coach_answer"
 # TODO: Implement the program that makes you discuss with your coach from the terminal.
+coach_answer("hello coach!")
+coach_answer("whats the time?")
+coach_answer("I am going to work right now!")
+coach_answer_enhanced("I AM GOING TO WORK RIGHT NOW")
