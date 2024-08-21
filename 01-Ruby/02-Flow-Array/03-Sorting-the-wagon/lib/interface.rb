e17d0537111372e283570f2dffd79fc9292b489c
szymon niedorezo <szymonniedorezo@gmail.com>
2024-08-21 15:08:22 +0100

    Add challenge solution

--- 01-Ruby/02-Flow-Array/03-Sorting-the-wagon/lib/interface.rb
+++ 01-Ruby/02-Flow-Array/03-Sorting-the-wagon/lib/interface.rb
@@ -5,7 +5,24 @@ require_relative "wagon_sort"
 #       - `gets`:  https://ruby-doc.org/core-3.1.2/Kernel.html#method-i-gets
 #       - `chomp`: https://ruby-doc.org/core-3.1.2/String.html#method-i-chomp
 
+puts "enter students name"
+students = []
+while true
+  input = gets.chomp
+  break if input.empty?
+  students << input
+end
+
 
 
 # TODO: Then call `wagon_sort` method defined in the wagon_sort.rb
 #       file and display the sorted student list
+sorted_students = wagon_sort(students)
+if sorted_students.length == 1
+  p "Congratulations! Your Wagon has #{sorted_students.length} student: #{sorted_students[0]}"
+else
+  and_students = sorted_students[-2..]
+  comma_students = sorted_students - and_students
+  p "Congratulations! Your Wagon has #{sorted_students.length} students:
+#{comma_students.join(', ')}, #{and_students.join(' and ')}"
+end
