2723fb36d60bd42a9cc9beb9bd464f9a58b25f3b
szymon niedorezo <szymonniedorezo@gmail.com>
2024-08-22 16:58:09 +0100

    Add solution to challenge

--- 01-Ruby/03-Iterators-Blocks/05-Splitter/lib/splitter.rb
+++ 01-Ruby/03-Iterators-Blocks/05-Splitter/lib/splitter.rb
@@ -2,6 +2,14 @@ def size_splitter(array, size)
   # TODO: Return an array of two arrays, the first containing
   #       words of length `size`, the second containing all the other words
   #       In addition to this split, each array should be *sorted*.
+  word_size = []
+  for word in array
+    if word.length == size
+      word_size << word
+    end
+  end
+  word_remain = array - word_size
+  return word_size.sort, word_remain.sort
 end
 
 def block_splitter(array)
@@ -9,4 +17,7 @@ def block_splitter(array)
   #       elements for which the given block yields true,
   #       the second containing all the other elements.
   #       No sort needed this time.
+  selected = array.select { |a| yield(a) }
+  arr_remain = array - selected
+  return [selected, arr_remain]
 end
