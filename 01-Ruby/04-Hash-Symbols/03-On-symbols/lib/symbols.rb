5c5dac91ebe9b6d97dcde6419799f70288b79742
szymon niedorezo <szymonniedorezo@gmail.com>
2024-08-23 14:42:20 +0100

    Add solution to challenge

--- 01-Ruby/04-Hash-Symbols/03-On-symbols/lib/symbols.rb
+++ 01-Ruby/04-Hash-Symbols/03-On-symbols/lib/symbols.rb
@@ -2,17 +2,19 @@
 # https://ruby-doc.org/core-3.1.2/BasicObject.html#method-i-__id__
 
 # You can try launching `irb` to test some values
-# irb> :foo.object_id
-# irb> :foo.object_id
-# irb> "foo".object_id
-# irb> "foo".object_id
+puts :foo.object_id
+puts :foo.object_id
+puts "foo".object_id
+puts "foo".object_id
 
 def are_identical_symbols_same_objects?
   # TODO: Answer the question by making this method to return true or false
+  return true
 end
 
 def are_identical_strings_same_objects?
   # TODO: Answer the question by making this method to return true or false
+  return false
 end
 
 
@@ -22,16 +24,20 @@ end
 
 def convert_string_to_symbol(a_string)
   # TODO: return the symbol version of the parameter `a_string` passed to this method
+  return a_string.to_sym
 end
 
 def convert_symbol_to_string(a_symbol)
   # TODO: return the string version of the parameter `a_symbol` passed to this method
+  return a_symbol.to_s
 end
 
 def me
   # TODO: return a Hash representing yourself, with keys such as age and name
+  return { name: "Simon", age: 37 }
 end
 
 def fruits
   # TODO: return an array of fruits
+  return ["banana", "apple", "grape"]
 end
