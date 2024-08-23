fb812258b911431fe0b97b37d2ebee3bbb269302
szymon niedorezo <szymonniedorezo@gmail.com>
2024-08-23 12:05:00 +0100

    Add solution to the challenge

--- 01-Ruby/04-Hash-Symbols/01-Modeling-with-Hash/lib/restaurant_order.rb
+++ 01-Ruby/04-Hash-Symbols/01-Modeling-with-Hash/lib/restaurant_order.rb
@@ -1,3 +1,22 @@
 def poor_calories_counter(burger, side, beverage)
   # TODO: return number of calories for this restaurant order
+  sum = 0
+  food = {
+  burger: {
+    "Hamburger" => 250,
+    "Cheese Burger" => 300,
+    "Veggie Burger" => 540,
+    "Vegan Burger" =>	350
+    },
+  side: {
+    "Sweet Potatoes" =>	230,
+    "Salad" => 15,
+  },
+  beverage: {
+    "Iced Tea"	=> 70,
+    "Lemonade"	=> 90
+  }
+  }
+  sum += food[:burger][burger] += food[:side][side] += food[:beverage][beverage]
+  return sum
 end
