11354071005c4f73eac4262b49ee5a378935313b
szymon niedorezo <szymonniedorezo@gmail.com>
2024-08-23 18:20:30 +0100

    Add partial solution

--- 01-Ruby/04-Hash-Symbols/05-Array-to-hash/lib/array_to_hash.rb
+++ 01-Ruby/04-Hash-Symbols/05-Array-to-hash/lib/array_to_hash.rb
@@ -2,7 +2,7 @@ def array_to_hash(array)
   # TODO: implement the method :)
   x = {}
   array.each_with_index { |value, index| x[index.to_s] = value }
-  (block_given? ? yield(index) : index)
+  #(block_given? ? yield(index) : index)
   return x
 end
 array = ["a", "b", "c"]
