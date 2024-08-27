a3c680d0fb26fd8ea10549f266c2ae625f3bd154
szymon niedorezo <szymonniedorezo@gmail.com>
2024-08-27 14:02:29 +0100

    Add solution to the challenge

--- 01-Ruby/06-Parsing/01-CSV-Parsing/lib/box_office.rb
+++ 01-Ruby/06-Parsing/01-CSV-Parsing/lib/box_office.rb
@@ -1,5 +1,18 @@
 require 'csv'
-
 def most_successful(number, max_year, file_path)
   # TODO: return an array of most successful movies before `max_year`
+  file_path = "/home/simon/code/Simon01001/fullstack-challenges/01-Ruby/06-Parsing/01-CSV-Parsing/lib/movies.csv"
+
+
+  filtered = []
+  CSV.foreach(file_path) do |row|
+    if row[1].to_i < max_year
+      filtered << { name: row[0], year: row[1].to_i, earnings: row[2].to_i }
+    end
+  end
+
+  sorted_list = filtered.sort_by do |movie|
+    -movie[:earnings]
+  end
+  return sorted_list[0...number]
 end
