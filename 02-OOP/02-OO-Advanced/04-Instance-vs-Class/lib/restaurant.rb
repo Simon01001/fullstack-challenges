575fe795680f6f2b20190c04cdef654555303dbe
szymon niedorezo <szymonniedorezo@gmail.com>
2024-08-29 15:08:03 +0100

    Add solution to challenge

--- 02-OOP/02-OO-Advanced/04-Instance-vs-Class/lib/restaurant.rb
+++ 02-OOP/02-OO-Advanced/04-Instance-vs-Class/lib/restaurant.rb
@@ -1,8 +1,24 @@
 class Restaurant
-  # TODO: add relevant accessors if necessary
+  attr_accessor :city, :name, :average_rating, :number_of_ratings, :sum_of_ratings
 
   def initialize(city, name)
-    # TODO: implement constructor with relevant instance variables
+    @city = city
+    @name = name
+    @average_rating = 0
+    @number_of_ratings = 0
+    @sum_of_ratings = 0
+  end
+
+  def rate(new_rate)
+    @number_of_ratings += 1
+    @sum_of_ratings += new_rate
+    @average_rating = @sum_of_ratings / @number_of_ratings
+  end
+
+  def self.filter_by_city(restaurants, city)
+    restaurants.select do |restaurant|
+      restaurant.city == city
+    end
   end
 
   # TODO: implement .filter_by_city and #rate methods
