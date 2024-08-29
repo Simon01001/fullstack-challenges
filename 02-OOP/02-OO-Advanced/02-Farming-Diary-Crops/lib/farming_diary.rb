966709f19c811c42bc4cee7fd3b8140bbea66306
szymon niedorezo <szymonniedorezo@gmail.com>
2024-08-29 14:19:20 +0100

    Add solution to challenge

--- 02-OOP/02-OO-Advanced/02-Farming-Diary-Crops/lib/farming_diary.rb
+++ 02-OOP/02-OO-Advanced/02-Farming-Diary-Crops/lib/farming_diary.rb
@@ -1,37 +1,38 @@
-# rubocop:disable Lint/LiteralInInterpolation
 # DO NOT remove the two lines below, needed for display purposes
 require_relative "../spec/helper/board"
 
 require_relative "corn"
 require_relative "rice"
+require_relative "crop"
 
 
 puts "\n\n📝 Day One: Corn"
 # 1. Instantiate a corn crop
+crop = Corn.new
+
+puts "The corn crop produced #{crop.grains} grains"
 
 # 2. Water the corn crop
-
+crop.water!
 # 3. Replace 'TODO' with the value of `grains` for your crop
-puts "The corn crop produced #{'TODO'} grains"
+puts "The corn crop produced #{crop.grains} grains"
 # 4. Replace 'TODO' with the state of the crop ('ripe' or 'not ripe')
-puts "The corn crop is #{'TODO'}"
+puts "The corn crop is #{crop.ripe?}"
 
 
 
 puts "\n\n📝 Day Two: Rice"
 # 1. Instantiate a rice crop
-
+crops = Rice.new
 # 2. Water the rice crop
-
+crops.water!
 # 3. Transplant the rice crop
-
+crops.transplant!
 # 4. Replace 'TODO' with the value of `grains` for your crop
-puts "The rice crop produced #{'TODO'} grains"
+puts "The rice crop produced #{crops.grains} grains"
 # 5. Replace 'TODO' with the state of the crop ('ripe' or 'not ripe')
-puts "The rice crop is #{'TODO'}"
+puts "The rice crop is #{crops.ripe?}"
 
 
 # DO NOT remove the two lines below, needed for display purposes
 Board.new.display
-
-# rubocop:enable Lint/LiteralInInterpolation
--- 02-OOP/02-OO-Advanced/02-Farming-Diary-Crops/lib/rice.rb
+++ 02-OOP/02-OO-Advanced/02-Farming-Diary-Crops/lib/rice.rb
@@ -1,1 +1,11 @@
-# TODO: code the Rice class
+require_relative 'crop'
+
+class Rice < Crop
+  def transplant!
+    @grains += 10
+  end
+
+  def water!
+    @grains += 5
+  end
+end
