6797849d2955e6d3c96ce94492970c7ff2c859e9
szymon niedorezo <szymonniedorezo@gmail.com>
2024-09-02 09:13:18 +0100

    Add solution to challenge

--- 02-OOP/03-Cookbook/03-Cookbook-With-CSV/lib/cookbook.rb
+++ 02-OOP/03-Cookbook/03-Cookbook-With-CSV/lib/cookbook.rb
@@ -3,9 +3,10 @@ require_relative "controller"
 require "csv"
 
 class Cookbook
-  def initialize(filepath)
-    @filepath = filepath
+  def initialize(csv_file)
+    @csv_file = csv_file
     @recipes = []
+    load_csv
   end
 
   #def save_csv(filepath)
@@ -20,9 +21,27 @@ class Cookbook
 
   def create(recipe)
     @recipes << recipe
+    save_csv
   end
 
   def destroy(index)
     @recipes.delete_at(index)
+    save_csv
+  end
+
+  private
+
+  def load_csv
+    CSV.foreach(@csv_file) do |row|
+      @recipes << Recipe.new(row[0], row[1])
+    end
+  end
+
+  def save_csv
+    CSV.open(@csv_file, "wb") do |csv|
+      @recipes.each do |recipe|
+        csv << [recipe.name, recipe.description]
+      end
+    end
   end
 end
