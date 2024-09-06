ce61eb1a97e777234edb61e5d5746c2607867b54
szymon niedorezo <szymonniedorezo@gmail.com>
2024-09-06 18:05:29 +0100

    Add solution to challenge

--- 03-AR-Database/03-ActiveRecord-Advanced/02-Validations/app/models/user.rb
+++ 03-AR-Database/03-ActiveRecord-Advanced/02-Validations/app/models/user.rb
@@ -7,4 +7,18 @@ class User < ActiveRecord::Base
   validates :email, presence: true
   validates :username, uniqueness: true
   validates :email, format: { with: /\A.*@.*\.com\z/ }
+  before_validation :strip_whitespace
+  after_create :send_welcome_email
+
+  def strip_whitespace
+    email&.strip!
+  end
+
+  def send_welcome_email
+    FakeMailer.instance.mail("bob@lebonge.com", "Welcome to HN!")
+  end
 end
+  #def welcome_email
+    #@user = user
+    #mail(to: @user.email, subject: "Welcome to this website")
+  #end
