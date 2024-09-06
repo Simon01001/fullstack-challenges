dddd23bee18de9cd08ece0b121ed7aed42a08089
szymon niedorezo <szymonniedorezo@gmail.com>
2024-09-06 12:03:22 +0100

    Add solution to challenge

--- 03-AR-Database/03-ActiveRecord-Advanced/01-Associations/db/seeds.rb
+++ 03-AR-Database/03-ActiveRecord-Advanced/01-Associations/db/seeds.rb
@@ -1,1 +1,9 @@
 # TODO: Write a seed
+require 'faker'
+
+5.times do
+  new_user = User.create(username: Faker::Name.name, email: Faker::Address)
+  rand(5..10).times do
+    new_user.posts << Post.create(title: Faker::Source, url: Faker::Internet, votes: Faker::Number)
+  end
+end
