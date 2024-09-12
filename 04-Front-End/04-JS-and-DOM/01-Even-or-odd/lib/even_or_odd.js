3bf2d9e44813c9c551116c7d2361e8896d878b95
szymon niedorezo <szymonniedorezo@gmail.com>
2024-09-12 11:28:53 +0100

    Add solution to challenge

--- 04-Front-End/04-JS-and-DOM/01-Even-or-odd/lib/even_or_odd.js
+++ 04-Front-End/04-JS-and-DOM/01-Even-or-odd/lib/even_or_odd.js
@@ -6,7 +6,12 @@ import runChallenges from "../spec/even_or_odd_examiner.js";
 
 export const evenOrOdd = (number) => {
   // TODO: this should return "even" if the number is even, "odd" otherwise
+  if (number % 2 === 1) {
+    return "odd";
+  }
+  return "even";
 };
+
 
 // Checking exercise answers. DO NOT MODIFY THIS LINE.
 runChallenges(evenOrOdd);
