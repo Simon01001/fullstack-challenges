10977d846fa12a14b01a67122bf13096f85b5c46
szymon niedorezo <szymonniedorezo@gmail.com>
2024-09-13 14:15:21 +0100

    Add solution to challenge

--- 04-Front-End/05-Events-and-Iterators/02-My-First-Event-Listener/lib/listener.js
+++ 04-Front-End/05-Events-and-Iterators/02-My-First-Event-Listener/lib/listener.js
@@ -1,1 +1,6 @@
 // TODO: React to a click on the button!
+const button = document.getElementById("clickme");
+button.addEventListener('click', () => {
+  button.setAttribute("disabled", true);
+  button.innerText = "Bingo!";
+});
