c9fada491aff0a1f42db783f678e8ed0346b1a53
szymon niedorezo <szymonniedorezo@gmail.com>
2024-09-13 13:11:25 +0100

    Add solution to challenge

--- 04-Front-End/05-Events-and-Iterators/01-Unordered-list/lib/list_generator.js
+++ 04-Front-End/05-Events-and-Iterators/01-Unordered-list/lib/list_generator.js
@@ -6,10 +6,17 @@ import runChallenges from "../spec/list_generator_examiner.js";
 
 const listItem = (content) => {
   // TODO: return the proper <li> HTML tag with its content (as a string)
+  return `<li class="list-group-item">${content}</li>`;
 };
 
 const unorderedList = (items) => {
   // TODO: return the proper <ul> markup (as a string)
+  let array = [];
+  items.forEach((item) => {
+    array.push(listItem(item));
+  });
+  console.log(`<ul class="list-group">${array.join("")}</ul>`);
+  return `<ul class="list-group">${array.join("")}</ul>`;
 };
 
 // Do not remove these lines:
@@ -17,6 +24,9 @@ if (typeof window === "object") {
   document.addEventListener("DOMContentLoaded", () => {
     // TODO: call unorderedList() passing the right element
     // TODO: display the list in the browser
+    const items = ["milk", "bread", "butter"];
+    const ul = unorderedList(items);
+    document.getElementById("list").insertAdjacentHTML("beforeend", ul);
   });
 }
 
