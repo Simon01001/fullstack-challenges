b611fbfe2ba912bef02b35726953e430a922b052
szymon niedorezo <szymonniedorezo@gmail.com>
2024-09-13 16:58:39 +0100

    Add partial soluion

--- 04-Front-End/05-Events-and-Iterators/03-Lottery-Scratchcards/lib/scratchcards.js
+++ 04-Front-End/05-Events-and-Iterators/03-Lottery-Scratchcards/lib/scratchcards.js
@@ -1,1 +1,19 @@
-// TODO: Write your gambling application here.
+const all = document.querySelectorAll("li");
+let balance = document.getElementById("balance");
+console.log(all);
+balance = parseInt(balance.innerText, 10);
+
+all.forEach((card) => {
+  card.addEventListener('click', (event) => {
+    balance -= 10;
+    console.log(balance);
+    const updatedBalance = document.getElementById("balance");
+    updatedBalance.innerText = balance;
+    if (balance < 10) {
+      balance = 10;
+    }
+    const prize = document.querySelector(".scratchcard");
+    balance += parseInt(prize.dataset.amount, 10);
+    prize.dataset.scratched = "true";
+  });
+});
