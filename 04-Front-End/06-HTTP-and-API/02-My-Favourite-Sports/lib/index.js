d714bd6c7304d9badb3c0512a5a4d167d040bc4d
szymon niedorezo <szymonniedorezo@gmail.com>
2024-09-16 12:15:34 +0100

    Add solution to challenge

--- 04-Front-End/06-HTTP-and-API/02-My-Favourite-Sports/lib/index.js
+++ 04-Front-End/06-HTTP-and-API/02-My-Favourite-Sports/lib/index.js
@@ -1,1 +1,16 @@
 // TODO: write your code here!
+const buttons = document.querySelectorAll(".clickable");
+//buttons.forEach((button) => {
+  //button.addEventListener("click", (event) => {
+    //event.currentTarget.classList.toggle("active");
+  //});
+//});
+const toggleActiveClass = (event) => {
+  event.currentTarget.classList.toggle("active");
+};
+const bindSportToClick = (button) => {
+  button.addEventListener("click", toggleActiveClass);
+};
+
+
+buttons.forEach(bindSportToClick);
