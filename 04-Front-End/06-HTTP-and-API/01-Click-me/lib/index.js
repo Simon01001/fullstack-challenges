a587bfd71ca59cb97f01f56c270d143d78f7d7ec
szymon niedorezo <szymonniedorezo@gmail.com>
2024-09-16 11:13:03 +0100

    Add solution to challenge

--- 04-Front-End/06-HTTP-and-API/01-Click-me/lib/index.js
+++ 04-Front-End/06-HTTP-and-API/01-Click-me/lib/index.js
@@ -2,6 +2,10 @@ const displayAlertOnButtonClick = () => {
   // TODO: Select the big green button
   // TODO: Bind the `click` event to the button
   // TODO: On click, display `Thank you!` in a JavaScript alert!
+  const button = document.querySelector(".btn");
+  button.addEventListener("click", (event) => {
+    window.alert("Thank you!");
+  });
 };
 
 displayAlertOnButtonClick(); // Do not remove!
