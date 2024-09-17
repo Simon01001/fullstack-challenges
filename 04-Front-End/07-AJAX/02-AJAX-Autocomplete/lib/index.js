b83c1c1cf95b89587ec4a94b96092df681260387
szymon niedorezo <szymonniedorezo@gmail.com>
2024-09-17 16:13:12 +0100

    Add solution to challenge

--- 04-Front-End/07-AJAX/02-AJAX-Autocomplete/lib/index.js
+++ 04-Front-End/07-AJAX/02-AJAX-Autocomplete/lib/index.js
@@ -1,1 +1,25 @@
 // TODO: Autocomplete the input with AJAX calls.
+const searchInput = document.getElementById("search");
+const resultsList = document.querySelector('#results');
+
+const displaySuggestions = (url) => {
+  fetch(url).then(response => response.json())
+    .then((data) => {
+      const suggestedWords = data.words;
+      suggestedWords.forEach((word) => {
+        resultsList.insertAdjacentHTML('beforeend', `<li>${word}</li>`);
+      });
+    });
+};
+
+searchInput.addEventListener("keyup", (event) => {
+  event.preventDefault();
+  resultsList.innerHTML = '';
+
+  const userInput = searchInput.value;
+
+  if (userInput) { // '' == false
+    const url = `https://dictionary.lewagon.com/autocomplete/${userInput}`;
+    displaySuggestions(url);
+  }
+});
