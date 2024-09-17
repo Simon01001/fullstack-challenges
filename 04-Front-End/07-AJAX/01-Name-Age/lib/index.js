63078115dd23e90320683581a6bbbb6aec1b4428
szymon niedorezo <szymonniedorezo@gmail.com>
2024-09-17 13:01:22 +0100

    Add solution to challenge

--- 04-Front-End/07-AJAX/01-Name-Age/lib/index.js
+++ 04-Front-End/07-AJAX/01-Name-Age/lib/index.js
@@ -1,11 +1,17 @@
 const apiUrl = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+const definition = document.getElementById("definition");
+const url = "https://api.dictionaryapi.dev/api/v2/entries/en/WORD";
 
 const displayDefinition = (event) => {
   event.preventDefault();
   const word = document.getElementById("word").value;
   // TODO: Display your word's definition with an AJAX call instead of the console.log()
   console.log(word);
+  fetch(url).then(response => response.json())
+    .then((data) => {
+      console.log(data);
+      definition.innerText = `${data[0].meanings[0].definitions[0].definition}`;
+    });
 };
-
 const form = document.getElementById("fetch-word");
 form.addEventListener("submit", displayDefinition);
