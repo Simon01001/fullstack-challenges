193aef373c438719b8f3f9b4b2d4f5f6e1a32d25
szymon niedorezo <szymonniedorezo@gmail.com>
2024-09-18 12:21:20 +0100

    Add solution to challenge

--- 04-Front-End/08-Templating/01-Vanilla-Todo-List-Warmup/lib/to-do-list.js
+++ 04-Front-End/08-Templating/01-Vanilla-Todo-List-Warmup/lib/to-do-list.js
@@ -4,11 +4,22 @@ const todos = [
   { title: "Do some exercise", done: false },
   { title: "Water the plants", done: true }
 ];
+const listContainer = document.getElementById("todosContainer");
+const checkbox = document.querySelector("input[type=checkbox]")
 
 // To-do item HTML 👇
-// <div class="shadow-sm rounded px-4 py-3 mb-2 border d-flex">
-//   <input class="d-flex form-check-input me-1" type="checkbox">
-//   <div>Replace with to-do title</div>
-// </div>
+todos.forEach((todo) => {
+  let check = "";
+  if (todo.done === true) {
+    check = "checked";
+  } else if (todo.done === false) {
+    check = "";
+  }
+  const listItem = `<div class="shadow-sm rounded px-4 py-3 mb-2 border d-flex">
+    <input class="d-flex form-check-input me-1" type="checkbox" ${check}>
+    <div>${todo.title}</div>
+  </div>`;
+  listContainer.insertAdjacentHTML("beforeend", `${listItem}`);
+});
 
 // TODO: Dynamically generate a list of to-dos based on `todos` array, and display them
