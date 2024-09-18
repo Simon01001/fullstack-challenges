7c76eb914913f7ce75db384e798af38f74941875
szymon niedorezo <szymonniedorezo@gmail.com>
2024-09-18 13:29:49 +0100

    Add solution to challenge

--- 04-Front-End/08-Templating/02-Vanilla-Todo-List/lib/to-do-list.js
+++ 04-Front-End/08-Templating/02-Vanilla-Todo-List/lib/to-do-list.js
@@ -4,5 +4,15 @@ const todos = [
   { title: "Do some exercise", done: false },
   { title: "Water the plants", done: true }
 ];
-
+const template = document.querySelector("#todoItemTemplate");
+const templateContainer = document.getElementById("todosContainer");
 // TODO: Use the template in the `index.html` to dynamically generate a list based on `todos` array
+
+todos.forEach((todo) => {
+  const clone = template.content.cloneNode(true);
+  clone.querySelector(".title").textContent = todo.title;
+  if (todo.done === false) {
+    clone.getElementById("box").removeAttribute("checked");
+  }
+  templateContainer.appendChild(clone);
+});
